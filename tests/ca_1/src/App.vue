52e28d0665b86f6341c943bccd76c1a43fc07713
zerbase <zerbase@naver.com>
2022-03-27 22:40:37 +0900

    vue 2

--- tests/ca_1/src/App.vue
+++ tests/ca_1/src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  HTML
+  <div>
+    <h4 >{{products[0]}}</h4>
+    <p>{{prices[0]}} 만원</p>
+  </div>
+  <div>
+    <h4 >{{products[1]}}</h4>
+    <p>{{prices[1]}} 만원</p>
+  </div>
+  <div>
+    <h4 >{{products[2]}}</h4>
+    <p>{{prices[2]}} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      // price1 : 80,
+      // price2 : 70,
+      // 스타일 : 'color : bule',
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      prices : [20,40,650],
+
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   } 
 }
 </script>
