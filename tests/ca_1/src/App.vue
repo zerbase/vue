8e24add8f963c26466fc749b26ecf8eca2009f8e
zerbase <zerbase@naver.com>
2022-03-30 00:27:15 +0900

    vue day4

--- tests/ca_1/src/App.vue
+++ tests/ca_1/src/App.vue
@@ -7,6 +7,9 @@
   <div v-for="(item, a) in products" :key="a"> 
     <h4 >{{products[a]}}</h4>
     <p>{{prices[a]}} 만원</p>
+    <button @click="increase(a)">허위매물신고</button> <span> 신고수 : {{신고수[a]}}</span>
+    <!-- v-on:click -->
+
   </div>
   <!-- <div>
     <h4 >{{products[1]}}</h4>
@@ -20,14 +23,22 @@
 
 <script>
 
+
+
 export default {
   name: 'App',
   data(){
     return {
-      메뉴들 : ['Home', 'Shop', '`About'],
+      신고수: [0,0,0],
+      메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
       prices : [20,40,650]
 
+    }
+  },
+  methods: {
+    increase(i) {
+      this.신고수[i] += 1
     }
   },
   components: {
