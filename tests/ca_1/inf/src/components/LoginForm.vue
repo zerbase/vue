8b044baeed0847f4a46afe644ed2da0a10d1eab1
zerbase <zerbase@naver.com>
2022-04-09 09:20:01 +0900

    vue inf

---
+++ tests/ca_1/inf/src/components/LoginForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+import { loginUser } from '@/api/index';
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+
+  methods: {
+    async submitForm() {
+      const userData = {
+        username: this.username,
+        password: this.password,
+      };
+      const response = await loginUser(userData);
+      console.log(response);
+    },
+  },
+};
+</script>
+
+<style></style>
