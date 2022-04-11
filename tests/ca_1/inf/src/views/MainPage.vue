2a23382770866a726a7b85e8c0ceea678b343504
zerbase <zerbase@naver.com>
2022-04-12 01:04:05 +0900

    vue inf day 6

--- tests/ca_1/inf/src/views/MainPage.vue
+++ tests/ca_1/inf/src/views/MainPage.vue
@@ -2,27 +2,39 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <ul>
-        <li v-for="postItem in postItems" :key="postItem._id">
-          {{ postItem.title }}
-        </li>
+      <LoadingSpinner v-if="isLoding"></LoadingSpinner>
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        ></PostListItem>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import { fetchPosts } from '@/api/index';
 export default {
+  components: {
+    PostListItem,
+    LoadingSpinner,
+  },
   data() {
     return {
       postItems: [],
+      isLoding: false,
     };
   },
   methods: {
     async fetchData() {
+      this.isLoding = true;
       const { data } = await fetchPosts();
-      console.log(data.posts);
+      this.isLoding = false;
+      // console.log(data.posts);
       this.postItems = data.posts;
     },
   },
