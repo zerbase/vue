dadb8cf37bbbc2e8cec7339f431ac7e9dbf81f78
zerbase <zerbase@naver.com>
2022-04-14 00:48:14 +0900

    vue inf day8

---
+++ tests/ca_1/inf/src/views/PostAddPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="form-container">
+    <PostAddForm></PostAddForm>
+  </div>
+</template>
+
+<script>
+import PostAddForm from '@/components/posts/postAddForm.vue';
+export default {
+  components: {
+    PostAddForm,
+  },
+};
+</script>
+
+<style></style>
